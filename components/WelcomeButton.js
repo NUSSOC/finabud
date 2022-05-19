5e1808007a5b96ec7a4c5912cf63ab79467a09e7
shittake <ericleowyuquan@gmail.com>
2022-05-19 17:08:32 +1000

    Upload cover photo for homepage

--- components/WelcomeButton.js
+++
@@ -1,8 +0,0 @@
-function WelcomeButton() {
-    return(<Button
-  title="Learn More"
-  color="#841584"
-  accessibilityLabel="Learn more about this purple button"
-  onPress={() => Alert.alert('Button with adjusted color pressed')}
-/>);
-}
